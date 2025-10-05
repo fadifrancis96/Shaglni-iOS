c0693c9844b798487a765dbd38c45a3cc213d61a
fadifrancis96 <fadifrancis96@users.noreply.github.com>
2025-10-05 15:45:05 +0300

    Initial Commit

---
+++ ShaglniTests/ShaglniTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShaglniTests.swift
+//  ShaglniTests
+//
+//  Created by Fadi Francis on 05/10/2025.
+//
+
+import Testing
+@testable import Shaglni
+
+struct ShaglniTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
